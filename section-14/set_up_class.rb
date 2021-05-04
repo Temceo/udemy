056c4adbbab9b6bdfa3994986d124d29d82a4ef7
temceo <martinmwaka@hotmail.com>
2021-05-04 21:29:02 +0100

    Complete section 14

---
+++ section-14/set_up_class.rb
@@ -0,0 +1,18 @@
+class Car
+  def initialize(make)
+    @make = make
+  end
+
+  def make
+    @make
+  end
+
+  def make=(make)
+    @make = make
+  end
+end
+
+new_car = Car.new("Honda")
+p new_car.make
+new_car.make = "Nissan"
+p new_car.make
